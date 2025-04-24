ef5e3dc773edcbcd3d0d25e426b6cafacfd1d27d
JamesTolputt <48034667+JamesTolputt@users.noreply.github.com>
2025-04-24 16:00:06 +1000

    #63 update AUTH implementation
    * #63 add port 3000 to backend/.env to work with Auth0
    
    * #63 fix - backend index.js now written in a ES6 compatible format
    
    * #63 delete - remove old webpage in backend/public folder
    
    * #63 fix - backend will now host astro dist on base path aka /backend/dist
    
    * #63 add - AuthJS via auth-astro plugin
    
    * #63 add - profile picture now changes to logged in image
    
    * #63 add - change dashboard to now display everything added via auth
    
    * #63 fix - change readme to reflect where astro build is to be put in backend
    
    * #63 fix - change readme to proper file level on dist

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,28 @@
-const express = require("express");
-const path = require("path");
-const dotenv = require("dotenv").config();
+import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import 'dotenv/config';
+
+// Astro import
+import { handler as ssrHandler } from '../dist/server/entry.mjs'
+
+// Get __dirname for setup because we are using ES6 modules by default
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
 // Serve the public directory
 app.use(express.static(path.join(__dirname, '../public/')));
 
-// Every other get will serve index.html
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+// Astro base directory
+const base = '/';
+app.use(base, express.static('dist/client/'));
+app.use(ssrHandler);
+
+// // Every other get will serve index.html
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
 
 app.listen(process.env.PORT, () => {
     console.log(`running server on ${process.env.PORT}.`);
--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,19 +1,41 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+// AlpineJS for declarative dynamic behaviour
 import alpinejs from '@astrojs/alpinejs';
-
+// CSS
 import tailwindcss from '@tailwindcss/vite';
+// Authentication
+import auth from 'auth-astro';
+// Adapter - Where are we hosting the output of astro
+import node from '@astrojs/node';
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs(
-    {
-      entrypoint: '/src/alpineEntryPoint'
-    }
-  )],
+  // Required for auth, makes it so the server does not pre-render by default
+  output: 'server',
+
+  // When we are building, we want something compatible with node
+  // 'middleware' means we can import the astro build as a middleware to express
+  adapter: node({
+    mode: 'middleware',
+  }),
+
+  integrations: [
+    alpinejs(
+      {
+        entrypoint: '/src/alpineEntryPoint'
+      }
+    ),
+    auth()
+  ],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  // Required for Auth0
+  server: {
+    port: 3000
+  }  
 });
