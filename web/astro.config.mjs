66e0c3349548f14d1de879daa7fe32f3db2b0316
JamesTolputt <48034667+JamesTolputt@users.noreply.github.com>
2025-04-28 23:24:29 +1000

    #64 Decouple Web frontend and backend
    Co-authored-by: athNdev <naphadeatharv@gmail.com>

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -4,27 +4,16 @@ import { defineConfig } from "astro/config";
 import alpinejs from "@astrojs/alpinejs";
 // CSS
 import tailwindcss from "@tailwindcss/vite";
-// Authentication
-import auth from "auth-astro";
-// Adapter - Where are we hosting the output of astro
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   // Required for auth, makes it so the server does not pre-render by default
-  output: "server",
-
-  // When we are building, we want something compatible with node
-  // 'middleware' means we can import the astro build as a middleware to express
-  adapter: node({
-    mode: "middleware",
-  }),
+  output: "static",
 
   integrations: [
     alpinejs({
       entrypoint: "/src/alpineEntryPoint",
-    }),
-    auth(),
+    })
   ],
 
   vite: {
