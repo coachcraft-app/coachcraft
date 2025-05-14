1a8ade3a1fdfab68ee61a1ef7b8397e1e01ea4c0
athNdev <naphadeatharv@gmail.com>
2025-05-15 02:14:26 +1000

    feat: #68 add working DropDown with a reactive list of ListButton

--- web/src/stores/activitiesStore.js
+++ web/src/stores/activitiesStore.js
@@ -1,106 +1,126 @@
-export function registerActivitiesStore(Alpine) {
-    Alpine.store('activities', {
+export default function activitiesStore(Alpine) {
+  Alpine.store("activities", {
+    // state
+    listsList: [
+      {
+        // Not dummy data, do not remove this default option
+        id: "default",
+        name: "All Activities",
+        activities: [],
+      },
+      {
+        id: "0",
+        name: "Warm Up",
+        activities: ["1", "2", "3", "4"],
+      },
+      {
+        id: "1",
+        name: "Dribbling Drills",
+        activities: ["5", "6", "7", "8"],
+      },
+      {
+        id: "2",
+        name: "Assorted",
+        activities: ["1", "5", "4", "7"],
+      },
+    ],
 
-        // state
-        listsList: [
-            {   // Not dummy data, do not remove this default option
-                "list_id": "0", 
-                "list_name": "All Activities",
-                "list_activities": [1, 2, 3, 4]
-            },
-            {
-                "list_id": "1", 
-                "list_name": "Warm Up",
-                "list_activities": [1, 2, 3, 4]
-            },
-            {
-                "list_id": "2", 
-                "list_name": "Dribbling Drills",
-                "list_activities": [5, 6, 7, 8]
-            },
-            {
-                "list_id": "3", 
-                "list_name": "Assorted",
-                "list_activities": [1, 5, 4, 7]
-            },
-        ],
+    activitiesList: [
+      {
+        id: "1",
+        name: "Catch Me If You Can",
+        duration: "00:30",
+        description: "",
+        notes: "",
+        img_url: "",
+      },
+      {
+        id: "2",
+        name: "Circle Passing",
+        duration: "00:45",
+        description: "",
+        notes: "",
+        img_url: "",
+      },
+      {
+        id: "3",
+        name: "Dribbling Commands",
+        duration: "00:30",
+        description: "",
+        notes: "",
+        img_url: "",
+      },
+      {
+        id: "4",
+        name: "Dynamic Soccer Warm Up",
+        duration: "01:00",
+        description: "",
+        notes: "",
+        img_url: "",
+      },
+      {
+        id: "5",
+        name: "1-on-1 Gate Dribbling",
+        duration: "01:00",
+        description: "",
+        notes: "",
+        img_url: "",
+      },
+      {
+        id: "6",
+        name: "1-on-1-on-1 Dribbling",
+        duration: "01:00",
+        description: "",
+        notes: "",
+        img_url: "",
+      },
+      {
+        id: "7",
+        name: "Dribble Attack",
+        duration: "01:00",
+        description: "",
+        notes: "",
+        img_url: "",
+      },
+      {
+        id: "8",
+        name: "Dribble Knockout",
+        duration: "01:00",
+        description: "",
+        notes: "",
+        img_url: "",
+      },
+    ],
+    selectedList: "default",
+    selectedActivity: "",
+    changesMade: false,
+    changesSyncInProgress: false,
 
-        activitiesList: [
-            {
-                "activity_id": "1", 
-                "activity_name": "Catch Me If You Can",
-                "duration": "00:30", 
-                "description": "", 
-                "notes": "",
-                "img_url": ""
-            },
-            {
-                "activity_id": "2", 
-                "activity_name": "Circle Passing",
-                "duration": "00:45", 
-                "description": "", 
-                "notes": "",
-                "img_url": ""
-            },
-            {
-                "activity_id": "3", 
-                "activity_name": "Dribbling Commands",
-                "duration": "00:30", 
-                "description": "", 
-                "notes": "",
-                "img_url": ""
-            },
-            {
-                "activity_id": "4", 
-                "activity_name": "Dynamic Soccer Warm Up",
-                "duration": "01:00", 
-                "description": "", 
-                "notes": "",
-                "img_url": ""
-            },
-            {
-                "activity_id": "5", 
-                "activity_name": "1-on-1 Gate Dribbling",
-                "duration": "01:00", 
-                "description": "", 
-                "notes": "",
-                "img_url": ""
-            },
-            {
-                "activity_id": "6", 
-                "activity_name": "1-on-1-on-1 Dribbling",
-                "duration": "01:00", 
-                "description": "", 
-                "notes": "",
-                "img_url": ""
-            },
-            {
-                "activity_id": "7", 
-                "activity_name": "Dribble Attack",
-                "duration": "01:00", 
-                "description": "", 
-                "notes": "",
-                "img_url": ""
-            },
-            {
-                "activity_id": "8", 
-                "activity_name": "Dribble Knockout",
-                "duration": "01:00", 
-                "description": "", 
-                "notes": "",
-                "img_url": ""
-            },
-        ],
-        selectedActivity: "",
-        selectedList: "0",
-        changesMade: false,
-        changesSyncInProgress: false,
+    get selectedListActivities() {
+      if (this.selectedList === "default") {
+        return this.activitiesList;
+      }
 
-        // fetch data from API
-        async fetchData() {
+      const selectedActivitiyIDs = this.listsList.find(
+        (list) => list.id === this.selectedList,
+      ).activities;
 
-        }
+      return this.activitiesList.filter((activity) =>
+        selectedActivitiyIDs.includes(activity.id),
+      );
+    },
 
-    });
-    // console.log("Activities List:", Alpine.store('activities').activitiesList);
+    get selectedListName() {
+      return this.listsList.find((list) => list.id === this.selectedList).name;
+    },
+
+    handleListSwitch(id) {
+      this.selectedList = id;
+    },
+
+    addNewActivity() {},
+
+    // fetch data from API
+    async fetchData() {},
+  });
 }
