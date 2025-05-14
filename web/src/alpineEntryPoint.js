f9fbf5efcb53daaeb0463f0116bb99d6b6b120c0
athNdev <naphadeatharv@gmail.com>
2025-05-15 00:59:15 +1000

    cleanup: remove unnecessary comments
    - astro.config.mjs
    - alpineEntryPoint.js

--- web/src/alpineEntryPoint.js
+++ web/src/alpineEntryPoint.js
@@ -1,15 +1,13 @@
-import { Alpine } from "alpinejs";
-import collapse from "@alpinejs/collapse"; // PenguinUI dependency
-import focus from "@alpinejs/focus"; // PenguinUI dependency
-import mask from "@alpinejs/mask"; // PenguinUI dependency
+import collapse from "@alpinejs/collapse";
+import focus from "@alpinejs/focus";
+import mask from "@alpinejs/mask";
 
-import { registerActivitiesStore } from "./stores/activitiesStore.js";
+import activitiesStore from "./stores/activitiesStore.js";
 
 export default function () {
-  // PenguinUI may require one or more of these for some components, hence they have been proactivly imported
   Alpine.plugin(collapse);
   Alpine.plugin(focus);
   Alpine.plugin(mask);
 
-  registerActivitiesStore(Alpine);
-}
+  activitiesStore(Alpine);
+};
