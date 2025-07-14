eb138ac6f0bb0bdcf8110fe463af29002068d1e2
athNdev <naphadeatharv@gmail.com>
2025-07-15 08:50:33 +1000

    update store imports in alpineEntryPoint.js

--- web/src/alpineEntryPoint.js
+++ web/src/alpineEntryPoint.js
@@ -3,9 +3,12 @@ import focus from "@alpinejs/focus";
 import mask from "@alpinejs/mask";
 
 import activitiesStore from "./stores/pages/activitiesStore.js";
-import schedulingStore from "./stores/schedulingStore.js";
-import teamsStore from "./stores/teamsStore.js";
-import sessionsStore from "./stores/sessionsStore.js";
+import schedulingStore from "./stores/pages/schedulingStore.js";
+import teamsStore from "./stores/pages/teamsStore.js";
+import sessionsStore from "./stores/pages/sessionsStore.js";
+import dashboardStore from "./stores/pages/dashboardStore.js";
+import notesStore from "./stores/pages/notesStore.js";
+import settingsStore from "./stores/pages/settingsStore.js";
 
 export default (Alpine) => {
   Alpine.plugin(collapse);
@@ -17,4 +20,7 @@ export default (Alpine) => {
   schedulingStore(Alpine);
   teamsStore(Alpine);
   sessionsStore(Alpine);
+  dashboardStore(Alpine);
+  notesStore(Alpine);
+  settingsStore(Alpine);
 };
