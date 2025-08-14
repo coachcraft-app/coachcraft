e0ec3f3ff17aed37a1517d4a1c5ef39d78011f4a
JamesTolputt <james.tolputt1@gmail.com>
2025-08-14 12:12:05 +1000

    Update: database schema now contains teams and players

--- backend/drizzle/0000_lean_la_nuit.sql
+++ backend/drizzle/0000_lean_la_nuit.sql
@@ -37,20 +37,31 @@ CREATE TABLE `list` (
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `list_name_unique` ON `list` (`name`);--> statement-breakpoint
-CREATE TABLE `note` (
+CREATE TABLE `players` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`text` text NOT NULL
+	`name` text NOT NULL,
+	`team` integer NOT NULL,
+	`lastModified` integer DEFAULT (unixepoch()) NOT NULL,
+	FOREIGN KEY (`team`) REFERENCES `teams`(`id`) ON UPDATE cascade ON DELETE cascade
 );
 --> statement-breakpoint
 CREATE TABLE `session` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`name` text NOT NULL,
-	`user` integer NOT NULL,
 	`date` integer NOT NULL,
 	`lastModified` integer DEFAULT (unixepoch()) NOT NULL,
-	`notes` integer NOT NULL,
-	FOREIGN KEY (`notes`) REFERENCES `note`(`id`) ON UPDATE cascade ON DELETE cascade
+	`notes` text NOT NULL,
+	`team` integer NOT NULL,
+	FOREIGN KEY (`team`) REFERENCES `teams`(`id`) ON UPDATE cascade ON DELETE cascade
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `session_name_unique` ON `session` (`name`);--> statement-breakpoint
-CREATE UNIQUE INDEX `session_date_unique` ON `session` (`date`);
+CREATE UNIQUE INDEX `session_date_unique` ON `session` (`date`);--> statement-breakpoint
+CREATE TABLE `teams` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`description` text,
+	`lastModified` integer DEFAULT (unixepoch()) NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `teams_name_unique` ON `teams` (`name`);
--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -18,90 +18,148 @@ import { sqliteTable, integer, text, check } from "drizzle-orm/sqlite-core";
 //     check("role_check", sql`${table.role} in ('coach', 'parent', 'anonymous')`)
 // ]);
 
+// A sports team
+export const teams = sqliteTable("teams", {
+  id: integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
+  name: text().notNull().unique(),
+  description: text(),
+  lastModified: integer({ mode: "timestamp" })
+    .notNull()
+    .default(sql`(unixepoch())`),
+});
+
+export const teamsRelations = relations(teams, ({ one, many }) => ({
+  session: many(sessions),
+  player: many(players),
+}));
+
 // A scheduled sports session
 export const sessions = sqliteTable("session", {
-    id: integer({ mode: 'number' }).primaryKey({ autoIncrement: true }),
-    name: text().notNull().unique(),
-    // user: integer( {mode: 'number' })
-    //     // .references(() => UsersTable.id, {onDelete: 'cascade', onUpdate: 'cascade'})
-    //     .notNull(),
-    date: integer({ mode: 'timestamp' }).unique().notNull(),
-    lastModified: integer({ mode: 'timestamp' })
-        .notNull()
-        .default(sql`(unixepoch())`),
-    notes: text().notNull()
+  id: integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
+  name: text().notNull().unique(),
+  // user: integer( {mode: 'number' })
+  //     // .references(() => UsersTable.id, {onDelete: 'cascade', onUpdate: 'cascade'})
+  //     .notNull(),
+  date: integer({ mode: "timestamp" }).unique().notNull(),
+  lastModified: integer({ mode: "timestamp" })
+    .notNull()
+    .default(sql`(unixepoch())`),
+  notes: text().notNull(),
+  team: integer({ mode: "number" })
+    .references(() => teams.id, { onDelete: "cascade", onUpdate: "cascade" })
+    .notNull(),
 });
 
 // Relations needed for findMany/First/etc.
 export const sessionsRelations = relations(sessions, ({ one, many }) => ({
-    activities: many(activities)
+  activities: many(activities),
+  teams: one(teams, {
+    fields: [sessions.team],
+    references: [teams.id],
+  }),
 }));
 
 // Activities that are part of a session
 export const activities = sqliteTable("activity", {
-    id: integer({ mode: 'number' }).primaryKey({ autoIncrement: true }),
-    session: integer({ mode: 'number' })
-        .references(() => sessions.id, {onDelete: 'cascade', onUpdate: 'cascade'})
-        .notNull(),
-    name: text().notNull().unique(),
-    description: text(),
-    duration: integer({ mode: 'number' }).notNull(),
-    imgUrl: text("img_url"),
-    lastModified: integer({ mode: 'timestamp' })
-        .notNull()
-        .default(sql`(unixepoch())`),
+  id: integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
+  session: integer({ mode: "number" })
+    .references(() => sessions.id, { onDelete: "cascade", onUpdate: "cascade" })
+    .notNull(),
+  name: text().notNull().unique(),
+  description: text(),
+  duration: integer({ mode: "number" }).notNull(),
+  imgUrl: text("img_url"),
+  lastModified: integer({ mode: "timestamp" })
+    .notNull()
+    .default(sql`(unixepoch())`),
 });
 
-export const activitiesRelations = relations(activities, ({one}) => ({
-    session: one(sessions, { fields: [activities.session], references: [sessions.id]})
+export const activitiesRelations = relations(activities, ({ one }) => ({
+  session: one(sessions, {
+    fields: [activities.session],
+    references: [sessions.id],
+  }),
+}));
+
+export const players = sqliteTable("players", {
+  id: integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  team: integer({ mode: "number" })
+    .references(() => teams.id, { onDelete: "cascade", onUpdate: "cascade" })
+    .notNull(),
+  lastModified: integer({ mode: "timestamp" })
+    .notNull()
+    .default(sql`(unixepoch())`),
+});
+
+export const playersRelations = relations(players, ({ one, many }) => ({
+  team: one(teams, {
+    fields: [players.team],
+    references: [teams.id],
+  }),
 }));
 
 // ----- ACTIVITY TEMPLATE CREATION -----
 // Activities that can be added to a session
 export const activityTemplates = sqliteTable("activity_template", {
-    id: integer({ mode: 'number' }).primaryKey({ autoIncrement: true }),
-    name: text().notNull().unique(),
-    description: text(),
-    duration: integer({ mode: 'number' }).notNull(),
-    imgUrl: text("img_url"),
-    lastModified: integer({ mode: 'timestamp' })
-        .notNull()
-        .default(sql`(unixepoch())`),
+  id: integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
+  name: text().notNull().unique(),
+  description: text(),
+  duration: integer({ mode: "number" }).notNull(),
+  imgUrl: text("img_url"),
+  lastModified: integer({ mode: "timestamp" })
+    .notNull()
+    .default(sql`(unixepoch())`),
 });
 
-export const activityTemplateRelations = relations(activityTemplates, ({many}) => ({
-    activityTemplatesToList: many(activityTemplateList)
-}));
+export const activityTemplateRelations = relations(
+  activityTemplates,
+  ({ many }) => ({
+    activityTemplatesToList: many(activityTemplateList),
+  })
+);
 
 // A list (or category) of activity templates for organisation
 export const lists = sqliteTable("list", {
-    id: integer({ mode: 'number' }).primaryKey({ autoIncrement: true }),
-    name: text().notNull().unique(),
-    accentColor: text(),
-    lastModified: integer({ mode: 'timestamp' })
-        .notNull()
-        .default(sql`(unixepoch())`),
+  id: integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
+  name: text().notNull().unique(),
+  accentColor: text(),
+  lastModified: integer({ mode: "timestamp" })
+    .notNull()
+    .default(sql`(unixepoch())`),
 });
 
-export const listRelations = relations(lists, ({many}) => ({
-    listToActivityTemplate: many(activityTemplateList)
+export const listRelations = relations(lists, ({ many }) => ({
+  listToActivityTemplate: many(activityTemplateList),
 }));
 
 // The many-to-many link between activities and lists
 export const activityTemplateList = sqliteTable("activity_template_list", {
-    id: integer({ mode: 'number' }).primaryKey({ autoIncrement: true }),
-    activityTemplate: integer( "activity_template", {mode: 'number' })
-        .references(() => activityTemplates.id, {onDelete: 'cascade', onUpdate: 'cascade'})
-        .notNull(),
-    list: integer({mode: 'number' })
-        .references(() => lists.id, {onDelete: 'cascade', onUpdate: 'cascade'})
-        .notNull(),
-    lastModified: integer({ mode: 'timestamp' })
-        .notNull()
-        .default(sql`(unixepoch())`),
+  id: integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
+  activityTemplate: integer("activity_template", { mode: "number" })
+    .references(() => activityTemplates.id, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    })
+    .notNull(),
+  list: integer({ mode: "number" })
+    .references(() => lists.id, { onDelete: "cascade", onUpdate: "cascade" })
+    .notNull(),
+  lastModified: integer({ mode: "timestamp" })
+    .notNull()
+    .default(sql`(unixepoch())`),
 });
 
-export const activityTemplateListRelations = relations(activityTemplateList, ({one}) => ({
-    activityTemplate: one(activityTemplates, { fields: [activityTemplateList.activityTemplate], references: [activityTemplates.id] }),
-    list: one(lists, { fields: [activityTemplateList.list], references: [lists.id]})
-}));
+export const activityTemplateListRelations = relations(
+  activityTemplateList,
+  ({ one }) => ({
+    activityTemplate: one(activityTemplates, {
+      fields: [activityTemplateList.activityTemplate],
+      references: [activityTemplates.id],
+    }),
+    list: one(lists, {
+      fields: [activityTemplateList.list],
+      references: [lists.id],
+    }),
+  })
+);
