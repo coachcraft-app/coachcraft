b80848a258e593c694b250bdec774bc149beb486
athNdev <55657682+athNdev@users.noreply.github.com>
2025-09-04 16:01:00 +0000

    style: prettify code

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -35,7 +35,7 @@ export const yoga = createYoga<{
 
 // Yoga needs this to avoid errors with multipart data
 app.addContentTypeParser("multipart/form-data", {}, (_req, _payload, done) =>
-  done(null)
+  done(null),
 );
 
 // add /graphql endpoint
