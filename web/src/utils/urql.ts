09ff061baf61263503834a6c2ecc88736cd19313
athNdev <naphadeatharv@gmail.com>
2025-10-05 03:41:44 +1100

    udpate urql setup to be a Class and add getToken()

--- web/src/utils/urql.ts
+++ web/src/utils/urql.ts
@@ -1,21 +1,38 @@
 import { fetchExchange, Client } from "@urql/core";
 import { cacheExchange } from "@urql/exchange-graphcache";
 import schema from "./schema.json";
+import Alpine from "alpinejs";
 
-// TODO: implement real token storage
-function getToken() {
-  return "";
+import type { AuthStore } from "../typeDefs/storeTypes";
+import type { User } from "oidc-client-ts";
+
+class urql {
+  // private access_token: string;
+  public urqlClient: Client | undefined;
+
+  private async getToken(): Promise<string | undefined> {
+    const authStore: AuthStore = Alpine.store("auth") as AuthStore;
+    const userManager = authStore.userManager;
+    const User: User | null = await userManager.getUser();
+    const access_token: string | undefined = User?.access_token;
+
+    return access_token;
+  }
+
+  constructor() {
+    this.getToken().then((access_token: string | undefined) => {
+      this.urqlClient = new Client({
+        url: "http://localhost:4500/graphql",
+        exchanges: [cacheExchange({ schema }), fetchExchange],
+        fetchOptions: () => {
+          const token = access_token;
+          return {
+            headers: { authorization: token ? `Bearer ${token}` : "" },
+          };
+        },
+      });
+    });
+  }
 }
 
-export const urqlClient = new Client({
-  url: "http://localhost:4500/graphql",
-  exchanges: [cacheExchange({ schema }), fetchExchange],
-  fetchOptions: () => {
-    const token = getToken();
-    return {
-      headers: { authorization: token ? `Bearer ${token}` : "" },
-    };
-  },
-});
-
-export default urqlClient;
+export default new urql();
