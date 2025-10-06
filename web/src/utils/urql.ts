c84aad2eb3029cfcf25af79feb8194974eab1002
athNdev <naphadeatharv@gmail.com>
2025-10-07 05:44:15 +1100

    refactor: replace urql constructor with an async init() method

--- web/src/utils/urql.ts
+++ web/src/utils/urql.ts
@@ -7,30 +7,28 @@ import type { AuthStore } from "../typeDefs/storeTypes";
 import type { User } from "oidc-client-ts";
 
 class urql {
-  // private access_token: string;
   public urqlClient: Client | undefined;
 
   private async getToken(): Promise<string | undefined> {
     const authStore: AuthStore = Alpine.store("auth") as AuthStore;
     const userManager = authStore.userManager;
     const User: User | null = await userManager.getUser();
-    const access_token: string | undefined = User?.access_token;
 
-    return access_token;
+    return User?.access_token;
   }
 
-  constructor() {
-    this.getToken().then((access_token: string | undefined) => {
-      this.urqlClient = new Client({
-        url: "http://localhost:4500/graphql",
-        exchanges: [cacheExchange({ schema }), fetchExchange],
-        fetchOptions: () => {
-          const token = access_token;
-          return {
-            headers: { authorization: token ? `Bearer ${token}` : "" },
-          };
-        },
-      });
+  public async init(): Promise<void> {
+    const accessToken: string | undefined = await this.getToken();
+
+    this.urqlClient = new Client({
+      url: "http://localhost:4500/graphql",
+      exchanges: [cacheExchange({ schema }), fetchExchange],
+      fetchOptions: () => {
+        const token = accessToken;
+        return {
+          headers: { authorization: token ? `Bearer ${token}` : "" },
+        };
+      },
     });
   }
 }
