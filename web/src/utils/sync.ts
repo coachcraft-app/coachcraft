3baa0fd88ffc0e8f778f754bdc9d30f558fa43c0
athNdev <naphadeatharv@gmail.com>
2025-10-07 16:12:02 +1100

    refactor: update sync.ts to make APIs more intuitive, add TSDoc comments

--- web/src/utils/sync.ts
+++ web/src/utils/sync.ts
@@ -4,38 +4,44 @@ import TeamsSync from "./teamsSync";
 import alpineInstance from "./alpineInstance";
 import type { PagesStore } from "../typeDefs/storeTypes";
 
+/**
+ * Sync statically exposes modular sync libs (activitiesSync, teamsSync, etc.)
+ *
+ * - No instantiation is necessary, Sync is stateless
+ * - The global urql instance is imported by all the modular sync libs
+ *
+ * - Accessing sync libs:
+ *  - import Sync from "sync.ts";
+ *
+ *    Sync.activities.list.put(<args>);
+ *    Sync.activities.activity.post(<args>);
+ *    Sync.teams.delete(<args>);
+ *    ..
+ *    ..
+ */
 class Sync {
-  bypassSync: boolean;
-  activities: {
-    activity: ActivitiesSync;
-    list: ActivitiesListsSync;
+  static activities = {
+    activity: new ActivitiesSync(),
+    list: new ActivitiesListsSync(),
   };
-  teams: TeamsSync;
+  static teams = new TeamsSync();
 
-  constructor(bypassSync: boolean = false) {
-    this.bypassSync = bypassSync;
-    const activity = new ActivitiesSync();
-    const list = new ActivitiesListsSync();
-    this.activities = {
-      activity,
-      list,
-    };
-    this.teams = new TeamsSync();
-
+  /**
+   * Sync activitiesList, listsList, teamsList to the server
+   */
+  static subscribeToStateLists(): void {
     const pagesStore: PagesStore = alpineInstance.instance.store(
       "pages",
     ) as PagesStore;
 
     // Subscribe to updates to activitiesList, listsLists, teamsList
-    if (!this.bypassSync) {
-      const activitiesList = pagesStore.activities?.activitiesList;
-      const listsList = pagesStore.activities?.listsList;
-      const teamsList = pagesStore.teams?.teamsList;
+    const activitiesList = pagesStore.activities?.activitiesList;
+    const listsList = pagesStore.activities?.listsList;
+    const teamsList = pagesStore.teams?.teamsList;
 
-      this.activities.activity.subscribeToActivitiesList(activitiesList);
-      this.activities.list.subscribeToListsList(listsList);
-      this.teams.subscribeToTeamsList(teamsList);
-    }
+    Sync.activities.activity.subscribeToActivitiesList(activitiesList);
+    Sync.activities.list.subscribeToListsList(listsList);
+    Sync.teams.subscribeToTeamsList(teamsList);
   }
 }
 
