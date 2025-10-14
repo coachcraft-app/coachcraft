1ad071b1148af7986ad150a1241f4d550de8ef40
athNdev <naphadeatharv@gmail.com>
2025-10-14 15:03:17 +1100

    add AUTH store type, cleanup

--- web/src/typeDefs/storeTypes.ts
+++ web/src/typeDefs/storeTypes.ts
@@ -1,45 +1,10 @@
-import type activities from "../stores/views/activities";
-import type scheduling from "../stores/views/scheduling";
-import type toast from "../stores/common/toast";
+import type { User, UserManager } from "oidc-client-ts";
 
 export type Player = string;
 
-// Represents a team used in teamsStore
-export interface Team {
-  id: string; // Contains string representation of int
-  name: string; // Name of team
-  description: string; // Description of team
-  players: Player[]; // Array of player names
-}
-
-// Represents an activity used in activitiesStore
-export interface Activity {
-  id: string; // Contains "default" or a string representation of int
-  name: string; // Name of activity
-  duration: string; // In the form "hh:mm"
-  description: string; //
-  img_url: string; //
-  lastModified?: Date; //
-}
-// Represents a list used in activitiesStore
-export interface ActivitiesList {
-  id: string;
-  name: string;
-  activities: string[]; // activity ids
-  accent_color: string;
-  lastModified?: Date;
-}
-export interface ActivitiesListAccentColors {
-  name: string;
-  hex: string;
-}
-
-export interface Session {
-  id: string;
-  name: string;
-  date: string;
-  notes: string;
-  activities: Activity[];
+export interface Auth {
+  user: User;
+  userManage: UserManager;
 }
 
 export interface PagesStore {
@@ -65,8 +30,34 @@ export interface PagesStore {
   };
 }
 
-export interface CommonStore {
-  toast: toast;
+// Represents an activity used in activitiesStore
+export interface Activity {
+  id: string; // Contains "default" or a string representation of int
+  name: string; // Name of activity
+  duration: string; // In the form "hh:mm"
+  description: string; //
+  img_url: string; //
+  lastModified?: Date; //
+}
+// Represents a list used in activitiesStore
+export interface ActivitiesList {
+  id: string;
+  name: string;
+  activities: string[]; // activity ids
+  accent_color: string;
+  lastModified?: Date;
+}
+export interface ActivitiesListAccentColors {
+  name: string;
+  hex: string;
+}
+
+// Represents a team used in teamsStore
+export interface Team {
+  id: string; // Contains string representation of int
+  name: string; // Name of team
+  description: string; // Description of team
+  players: Player[]; // Array of player names
 }
 
 export interface Session {
