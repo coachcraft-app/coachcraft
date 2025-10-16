86b304aa7dfe056bc8369764c461fe39f020f881
James Tolputt <james.tolputt1@gmail.com>
2025-10-16 15:44:39 +1100

    update: comment and export auth

--- web/src/stores/auth.ts
+++ web/src/stores/auth.ts
@@ -1,4 +1,14 @@
-export default class auth {
+/**
+ * Auth store to hold user information
+ * Used by the alpine singleton class
+ *
+ * @module
+ */
+
+/**
+ * Auth store type definition
+ */
+export class auth {
   public userProfilePic: string | undefined;
   public givenName: string | undefined;
   public userEmail: string | undefined;
@@ -8,3 +18,5 @@ export default class auth {
    */
   public constructor() {}
 }
+
+export default auth;
