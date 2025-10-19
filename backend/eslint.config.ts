5e0978714926dddd006c68aaae240207a57756f6
athNdev <naphadeatharv@gmail.com>
2025-10-19 23:31:05 +1100

    feature: add eslint test reporting with unicorn in /backend

--- backend/eslint.config.ts
+++ backend/eslint.config.ts
@@ -1,14 +1,55 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+// @ts-nocheck
 
-export default defineConfig([
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+import { fileURLToPath } from "url";
+import * as path from "path";
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default tseslint.config([
+  ...tseslint.configs.recommended,
+
+  // Unicorn recommended preset
+  pluginUnicorn.configs["flat/recommended"],
+
+  // Global rule tweaks
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    rules: {
+      // Patch to fix known issues with ESLint, Unicorn combo
+      "unicorn/expiring-todo-comments": "off",
+    },
   },
-  tseslint.configs.recommended,
+
+  // Project settings
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: true, // makes eslint "type aware"
+        tsconfigRootDir: dirname,
+        sourceType: "module",
+      },
+      globals: globals.node, // Node.js environment globals types
+    },
+  },
+
+  // Ignores
+  {
+    ignores: [
+      "**/*.js",
+      "**/*.mjs",
+      "**/node_modules/**",
+      "dist/**",
+      ".astro/**",
+      "coverage/**",
+      "cypress/**",
+      "cypress.config.*",
+      "astro.config.ts",
+      "tailwind.config.ts",
+      "drizzle.config.ts",
+      "eslint.config.ts",
+    ],
+  },
 ]);
